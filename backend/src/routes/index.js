487a882fdd606a3e017744a2887dbee22054f8fb
Alexis <you@example.com>
2023-03-16 11:55:25 -0600

    changes

--- backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const produts = require("./products/products-Routes");
-
-const prefixApi = "/api";
-
-function routerApi(app) {
-  app.use(`${prefixApi}/all-product`, produts);
-}
-
-module.exports = routerApi;
