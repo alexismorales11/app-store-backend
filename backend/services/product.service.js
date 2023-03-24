249ecef92b651efeab649110d537dd3a21a4a264
Alexis Morales <selvinmoralesoficial@gmail.com>
2023-03-24 08:01:27 -0600

    POST, GET, PATCH, DELETE

--- backend/services/product.service.js
+++ backend/services/product.service.js
@@ -51,11 +51,8 @@ class ProductsService {
   }
 
   async delete(id) {
-    const index = this.products.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw boom.notFound('product not found');
-    }
-    this.products.splice(index, 1);
+    const query =   `DELETE FROM TASK WHERE ID = ${id}`;
+    await sequelize.query(query);
     return { id };
   }
 }
